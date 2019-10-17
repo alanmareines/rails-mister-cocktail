93424f6ea89cd021f15c9533cb0ae88bb8e15615
Alan Mareines <alanmareines@gmail.com>
2019-10-17 18:53:01 -0300

    trying to modal

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,6 +3,11 @@ class CocktailsController < ApplicationController
 
   def index
     @cocktails = Cocktail.all
+    @search = params['search']
+    if @search.present?
+      @name = @search['cocktail_name']
+      @cocktails = Cocktail.where('name ILIKE ?', "%#{@name}%")
+    end
   end
 
   def show
